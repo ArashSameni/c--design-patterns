61aeafc200cedb22f7ccd7160fb0e8fe142b1a71
arash <arashsameniy@gmail.com>
2022-08-23 19:12:04 +0430

    Use connection class in facade pattern

--- DesignPatterns/Structural/Facade/NotificationServer/NotificationServer.cs
+++ DesignPatterns/Structural/Facade/NotificationServer/NotificationServer.cs
@@ -2,16 +2,17 @@ namespace DesignPatterns.Structural.Facade.NotificationServer;
 
 public class NotificationServer
 {
-    private readonly string _ipAddress;
-
+    private readonly Connection _connection;
     public NotificationServer(string ipAddress, string appId, string key)
     {
-        _ipAddress = ipAddress;
+        _connection = new Connection(ipAddress);
         new AuthToken(appId, key).Authenticate();
     }
 
     public void Send(Message message, string to)
     {
+        _connection.Connect();
         Console.WriteLine($"Sending {message.Text} to {to}");
+        _connection.Disconnect();
     }
 }
